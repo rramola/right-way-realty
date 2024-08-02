42a90481647d97a81e7f2326d01c5a8cc0bcd40f
RRamola <ramolaryan@gmail.com>
2024-08-01 21:12:43 -0500

    Created Starter Templates, Styles, Views, and URL's

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    address = models.CharField(max_length=255)
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=4, decimal_places=2)
+    sqft = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+
+class PropertyImage(models.Model):
+    property = models.ForeignKey(
+        Property, related_name="images", on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to="property_images/")
+
+    def __str__(self):
+        return f"Image for {self.property.title}"
