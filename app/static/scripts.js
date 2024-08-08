51fa87230fa888f02e6247e0992ffe1b33073ccd
FraicheSolfege <littlenathan68@gmail.com>
2024-08-07 21:59:58 -0500

    DIVS???

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -6,3 +6,25 @@ function toggleNavbar() {
         navbar.style.width = '200px';
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+
+    const options = {
+        threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
