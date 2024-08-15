db070b347a1dc06e031957631371209817385520
rramola <ramolaryan@gmail.com>
2024-08-14 23:57:13 -0500

    test

--- right_way_realty/settings.py
+++ right_way_realty/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=600)
