2a1b19b0c75c8811b357509698c2400ee17ed629
Ryan Ramola <50272454+rramola@users.noreply.github.com>
2024-10-28 11:06:22 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -75,65 +75,69 @@ class ContactForm(forms.Form):
 
 
 class UserRegisterForm(UserCreationForm):
-    username = form.CharField(
-        max_length=15,
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                "id": "username-container",
-                "class": "form_group",
-                "placeholder": "Username",
-            }
-        ),
-    )
-    first_name = forms.CharField(
-        max_length=30,
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                "id": "first-name-container",
-                "class": "form_group",
-                "placeholder": "First Name",
-            }
-        ),
-    )
-    last_name = forms.CharField(
-        max_length=30,
-        required=True,
-        widget=forms.TextInput(
-            attrs={
-                "id": "last-name-container",
-                "class": "form_group",
-                "placeholder": "Last Name",
-            }
-        ),
-    )
-    phone_number = forms.CharField(
-        required=True,
-        validators=[
-            RegexValidator(
-                regex=r"^\d{3}-\d{3}-\d{4}$",
-                message="Enter a valid phone number (e.g., 123-456-7890).",
-            )
-        ],
-        widget=forms.TextInput(
-            attrs={
-                "id": "phone-number-container",
-                "class": "form_group",
-                "placeholder": "123-456-7890",
-            }
-        ),
-    )
-    email = forms.EmailField(
-        required=True,
-        widget=forms.EmailInput(
-            attrs={
-                "id": "email-container",
-                "class": "form_group",
-                "placeholder": "Email",
-            }
-        ),
-    )
+    email = forms.EmailField(required=True)
+    phone_number = forms.CharField(max_length=20, required=True)
+    first_name = forms.CharField(max_length=20, required=True)
+    last_name = forms.CharField(max_length=20, required=True)
+    # username = form.CharField(
+    #     max_length=15,
+    #     required=True,
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "id": "username-container",
+    #             "class": "form_group",
+    #             "placeholder": "Username",
+    #         }
+    #     ),
+    # )
+    # first_name = forms.CharField(
+    #     max_length=30,
+    #     required=True,
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "id": "first-name-container",
+    #             "class": "form_group",
+    #             "placeholder": "First Name",
+    #         }
+    #     ),
+    # )
+    # last_name = forms.CharField(
+    #     max_length=30,
+    #     required=True,
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "id": "last-name-container",
+    #             "class": "form_group",
+    #             "placeholder": "Last Name",
+    #         }
+    #     ),
+    # )
+    # phone_number = forms.CharField(
+    #     required=True,
+    #     validators=[
+    #         RegexValidator(
+    #             regex=r"^\d{3}-\d{3}-\d{4}$",
+    #             message="Enter a valid phone number (e.g., 123-456-7890).",
+    #         )
+    #     ],
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "id": "phone-number-container",
+    #             "class": "form_group",
+    #             "placeholder": "123-456-7890",
+    #         }
+    #     ),
+    # )
+    # email = forms.EmailField(
+    #     required=True,
+    #     widget=forms.EmailInput(
+    #         attrs={
+    #             "id": "email-container",
+    #             "class": "form_group",
+    #             "placeholder": "Email",
+    #         }
+    #     ),
+    # )
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_number', 'first_name', 'last_name', 'password1', 'password2']
