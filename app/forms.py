a5f39c029f90b736361d6181277de8401cdfa3a8
Ryan Ramola <50272454+rramola@users.noreply.github.com>
2024-10-28 10:13:14 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -75,10 +75,54 @@ class ContactForm(forms.Form):
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    phone_number = forms.CharField(max_length=20, required=True)
-    first_name = forms.CharField(max_length=20, required=True)
-    last_name = forms.CharField(max_length=20, required=True)
+    first_name = forms.CharField(
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "contact_us_first_name",
+                "class": "contact_us_form_group",
+                "placeholder": "First Name",
+            }
+        ),
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "id": "contact_us_last_name",
+                "class": "contact_us_form_group",
+                "placeholder": "Last Name",
+            }
+        ),
+    )
+    phone_number = forms.CharField(
+        required=True,
+        validators=[
+            RegexValidator(
+                regex=r"^\d{3}-\d{3}-\d{4}$",
+                message="Enter a valid phone number (e.g., 123-456-7890).",
+            )
+        ],
+        widget=forms.TextInput(
+            attrs={
+                "id": "contact_us_phone_number",
+                "class": "contact_us_form_group",
+                "placeholder": "123-456-7890",
+            }
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "id": "contact_us_email",
+                "class": "contact_us_form_group",
+                "placeholder": "Email",
+            }
+        ),
+    )
     class Meta:
         model = User
         fields = ['username', 'email', 'phone_number', 'first_name', 'last_name', 'password1', 'password2']
