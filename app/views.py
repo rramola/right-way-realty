1f1090afbbe4e000bbab9df2f3d7bb24b8109d34
FraicheSolfege <154850011+FraicheSolfege@users.noreply.github.com>
2024-10-30 19:32:59 -0500

    Tweaked Rental Stylings / added pagination, normalized home header texts

--- app/views.py
+++ app/views.py
@@ -16,6 +16,7 @@ from django.urls import reverse
 from django.db import connection
 from datetime import date
 from django.contrib.auth import logout
+from django.core.paginator import Paginator
 
 
 # from populate_dummy_data import *
@@ -169,6 +170,9 @@ def contact_page(request):
 
 def rental_list(request):
     rentals = Rental.objects.all()
+    paginator = Paginator(rentals, 5)
+    page_number = request.GET.get('page')
+    rentals = paginator.get_page(page_number)
     return render(request, 'rental_list.html', {'rentals': rentals})
 
 def load_more_properties(request):
